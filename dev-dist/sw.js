271d42d1eb419d8e6b002d6950fd59a539f93999
mkyousafali <mk.yousafali@gmail.com>
2026-03-30 20:55:53 +0530

    Fix working hours calculation in shift assignment modal - remove buffer deduction

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b4fc5514'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.5kactjljpdg"
+    "revision": "0.hislsqba30o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
